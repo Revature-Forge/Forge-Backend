d10b436802e8aa57f32b15abe38dc74040f573b8
James Butler <m.james.butler@gmail.com>
2021-07-27 09:29:26 -0500

    Jacoco configuration reset.

--- src/test/java/com/forge/revature/demo/GitHubControllerTest.java
+++ src/test/java/com/forge/revature/demo/GitHubControllerTest.java
@@ -18,7 +18,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.http.MediaType;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.mockito.Mockito;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import static org.hamcrest.Matchers.*;
