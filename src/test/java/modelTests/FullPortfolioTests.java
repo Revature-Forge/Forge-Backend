af905fce1b53651b71ce96f49652fd261defae93
James Butler <m.james.butler@gmail.com>
2021-07-27 14:24:03 -0500

    typo

--- src/test/java/modelTests/FullPortfolioTests.java
+++ src/test/java/modelTests/FullPortfolioTests.java
@@ -12,28 +12,14 @@ class FullPortfolioTests {
 
 @Test
 void constructorTest() {
-//	this.name = name;
-//	this.submitted = submitted;
-//	this.approved = approved;
-//	this.reviewed = reviewed;
-//	this.feedback = feedback;
-//	this.aboutMe = aboutMe;
-//	this.certifications = certifications;
-//	this.educations = educations;
-//	this.equivalencies = equivalencies;
-//	this.gitHubs = gitHubs;
-//	this.honors = honors;
-//	this.projects = projects;
-//	this.workExperiences = workExperiences;
-//	this.workHistories = workHistories;
 	
-	FullPortfolio p = new FullPortfolio("test", true, false, false, "feeback", null, null, null, null, null, null, null, null, null);
+	FullPortfolio p = new FullPortfolio("test", true, false, false, "feedback", null, null, null, null, null, null, null, null, null);
 	assertSame("feedback", p.getFeedback());
 }
 
 @Test
 void toStringTest() {
-	FullPortfolio p = new FullPortfolio("test", true, false, false, "feeback", null, null, null, null, null, null, null, null, null);
+	FullPortfolio p = new FullPortfolio("test", true, false, false, "feedback", null, null, null, null, null, null, null, null, null);
 	assertNotNull(p.toString());
 	assertTrue(p.toString().length() > 0);
 }
