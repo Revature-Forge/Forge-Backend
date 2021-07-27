054ad030e7f18b5de9ac192eb38360b80e0c49e9
ajang06 <aronjang97@gmail.com>
2021-07-27 12:10:22 -0500

    Revert "Revert "Added Flagging fields to portfolio and full portfolio""
    This reverts commit c1f3fa6f8234466e713eba0b7e1ec850107807f9.

---
+++ src/main/java/com/forge/revature/converter/HashMapConverter.java
@@ -0,0 +1,39 @@
+package com.forge.revature.converter;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.persistence.AttributeConverter;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class HashMapConverter implements AttributeConverter<Map<String, String>, String> {
+
+    @Override
+    public String convertToDatabaseColumn(Map<String, String> flag) {
+    	ObjectMapper objectMapper = new ObjectMapper();
+        String flagInfoJson = null;
+        try {
+            flagInfoJson = objectMapper.writeValueAsString(flag);
+        } catch (final JsonProcessingException e) {
+            //logger.error("JSON writing error", e);
+        }
+
+        return flagInfoJson;
+    }
+
+    @Override
+    public Map<String, String> convertToEntityAttribute(String flagInfoJSON) {
+    	ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, String> flagInfo = null;
+        try {
+            flagInfo = objectMapper.readValue(flagInfoJSON, Map.class);
+        } catch (final IOException e) {
+            //logger.error("JSON reading error", e);
+        }
+
+        return flagInfo;
+    }
+
+}
--- src/main/java/com/forge/revature/models/FullPortfolio.java
+++ src/main/java/com/forge/revature/models/FullPortfolio.java
@@ -1,5 +1,6 @@
 package com.forge.revature.models;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class FullPortfolio {
@@ -10,6 +11,7 @@ public class FullPortfolio {
     private boolean approved;
     private boolean reviewed;
     private String feedback;
+    private HashMap<String,String> flags;
     private AboutMe aboutMe;
     private List<Certification> certifications;
     private List<Education> educations;
@@ -24,161 +26,172 @@ public class FullPortfolio {
     }
 
     public FullPortfolio(int id, String name, User user, boolean submitted, boolean approved, boolean reviewed,
-            String feedback, AboutMe aboutMe, List<Certification> certifications, List<Education> educations,
-            List<Equivalency> equivalencies, List<GitHub> gitHubs, List<Honor> honors, List<Project> projects,
-            List<WorkExperience> workExperiences, List<WorkHistory> workHistories) {
-        this.id = id;
-        this.name = name;
-        this.user = user;
-        this.submitted = submitted;
-        this.approved = approved;
-        this.reviewed = reviewed;
-        this.feedback = feedback;
-        this.aboutMe = aboutMe;
-        this.certifications = certifications;
-        this.educations = educations;
-        this.equivalencies = equivalencies;
-        this.gitHubs = gitHubs;
-        this.honors = honors;
-        this.projects = projects;
-        this.workExperiences = workExperiences;
-        this.workHistories = workHistories;
-    }
+			String feedback, HashMap<String, String> flags, AboutMe aboutMe, List<Certification> certifications,
+			List<Education> educations, List<Equivalency> equivalencies, List<GitHub> gitHubs, List<Honor> honors,
+			List<Project> projects, List<WorkExperience> workExperiences, List<WorkHistory> workHistories) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.user = user;
+		this.submitted = submitted;
+		this.approved = approved;
+		this.reviewed = reviewed;
+		this.feedback = feedback;
+		this.flags = flags;
+		this.aboutMe = aboutMe;
+		this.certifications = certifications;
+		this.educations = educations;
+		this.equivalencies = equivalencies;
+		this.gitHubs = gitHubs;
+		this.honors = honors;
+		this.projects = projects;
+		this.workExperiences = workExperiences;
+		this.workHistories = workHistories;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public User getUser() {
-        return user;
-    }
+	public User getUser() {
+		return user;
+	}
 
-    public void setUser(User user) {
-        this.user = user;
-    }
+	public void setUser(User user) {
+		this.user = user;
+	}
 
-    public boolean isSubmitted() {
-        return submitted;
-    }
+	public boolean isSubmitted() {
+		return submitted;
+	}
 
-    public void setSubmitted(boolean submitted) {
-        this.submitted = submitted;
-    }
+	public void setSubmitted(boolean submitted) {
+		this.submitted = submitted;
+	}
 
-    public boolean isApproved() {
-        return approved;
-    }
+	public boolean isApproved() {
+		return approved;
+	}
 
-    public void setApproved(boolean approved) {
-        this.approved = approved;
-    }
+	public void setApproved(boolean approved) {
+		this.approved = approved;
+	}
 
-    public boolean isReviewed() {
-        return reviewed;
-    }
+	public boolean isReviewed() {
+		return reviewed;
+	}
 
-    public void setReviewed(boolean reviewed) {
-        this.reviewed = reviewed;
-    }
+	public void setReviewed(boolean reviewed) {
+		this.reviewed = reviewed;
+	}
 
-    public String getFeedback() {
-        return feedback;
-    }
+	public String getFeedback() {
+		return feedback;
+	}
 
-    public void setFeedback(String feedback) {
-        this.feedback = feedback;
-    }
+	public void setFeedback(String feedback) {
+		this.feedback = feedback;
+	}
 
-    public AboutMe getAboutMe() {
-        return aboutMe;
-    }
+	public HashMap<String, String> getFlags() {
+		return flags;
+	}
 
-    public void setAboutMe(AboutMe aboutMe) {
-        this.aboutMe = aboutMe;
-    }
+	public void setFlags(HashMap<String, String> flags) {
+		this.flags = flags;
+	}
 
-    public List<Certification> getCertifications() {
-        return certifications;
-    }
+	public AboutMe getAboutMe() {
+		return aboutMe;
+	}
 
-    public void setCertifications(List<Certification> certifications) {
-        this.certifications = certifications;
-    }
+	public void setAboutMe(AboutMe aboutMe) {
+		this.aboutMe = aboutMe;
+	}
 
-    public List<Education> getEducations() {
-        return educations;
-    }
+	public List<Certification> getCertifications() {
+		return certifications;
+	}
 
-    public void setEducations(List<Education> educations) {
-        this.educations = educations;
-    }
+	public void setCertifications(List<Certification> certifications) {
+		this.certifications = certifications;
+	}
 
-    public List<Equivalency> getEquivalencies() {
-        return equivalencies;
-    }
+	public List<Education> getEducations() {
+		return educations;
+	}
 
-    public void setEquivalencies(List<Equivalency> equivalencies) {
-        this.equivalencies = equivalencies;
-    }
+	public void setEducations(List<Education> educations) {
+		this.educations = educations;
+	}
 
-    public List<GitHub> getGitHubs() {
-        return gitHubs;
-    }
+	public List<Equivalency> getEquivalencies() {
+		return equivalencies;
+	}
 
-    public void setGitHubs(List<GitHub> gitHubs) {
-        this.gitHubs = gitHubs;
-    }
+	public void setEquivalencies(List<Equivalency> equivalencies) {
+		this.equivalencies = equivalencies;
+	}
 
-    public List<Honor> getHonors() {
-        return honors;
-    }
+	public List<GitHub> getGitHubs() {
+		return gitHubs;
+	}
 
-    public void setHonors(List<Honor> honors) {
-        this.honors = honors;
-    }
+	public void setGitHubs(List<GitHub> gitHubs) {
+		this.gitHubs = gitHubs;
+	}
 
-    public List<Project> getProjects() {
-        return projects;
-    }
+	public List<Honor> getHonors() {
+		return honors;
+	}
 
-    public void setProjects(List<Project> projects) {
-        this.projects = projects;
-    }
+	public void setHonors(List<Honor> honors) {
+		this.honors = honors;
+	}
 
-    public List<WorkExperience> getWorkExperiences() {
-        return workExperiences;
-    }
+	public List<Project> getProjects() {
+		return projects;
+	}
 
-    public void setWorkExperiences(List<WorkExperience> workExperiences) {
-        this.workExperiences = workExperiences;
-    }
+	public void setProjects(List<Project> projects) {
+		this.projects = projects;
+	}
 
-    public List<WorkHistory> getWorkHistories() {
-        return workHistories;
-    }
+	public List<WorkExperience> getWorkExperiences() {
+		return workExperiences;
+	}
 
-    public void setWorkHistories(List<WorkHistory> workHistories) {
-        this.workHistories = workHistories;
-    }
+	public void setWorkExperiences(List<WorkExperience> workExperiences) {
+		this.workExperiences = workExperiences;
+	}
 
-    @Override
-    public String toString() {
-        return "FullPortfolio [aboutMe=" + aboutMe + ", approved=" + approved + ", certifications=" + certifications
-                + ", educations=" + educations + ", equivalencies=" + equivalencies + ", feedback=" + feedback
-                + ", gitHubs=" + gitHubs + ", honors=" + honors + ", id=" + id + ", name=" + name + ", projects="
-                + projects + ", reviewed=" + reviewed + ", submitted=" + submitted + ", user=" + user
-                + ", workExperiences=" + workExperiences + ", workHistories=" + workHistories + "]";
-    }
+	public List<WorkHistory> getWorkHistories() {
+		return workHistories;
+	}
+
+	public void setWorkHistories(List<WorkHistory> workHistories) {
+		this.workHistories = workHistories;
+	}
+
+	@Override
+	public String toString() {
+		return "FullPortfolio [id=" + id + ", name=" + name + ", user=" + user + ", submitted=" + submitted
+				+ ", approved=" + approved + ", reviewed=" + reviewed + ", feedback=" + feedback + ", flags=" + flags
+				+ ", aboutMe=" + aboutMe + ", certifications=" + certifications + ", educations=" + educations
+				+ ", equivalencies=" + equivalencies + ", gitHubs=" + gitHubs + ", honors=" + honors + ", projects="
+				+ projects + ", workExperiences=" + workExperiences + ", workHistories=" + workHistories + "]";
+	}
+
 }
