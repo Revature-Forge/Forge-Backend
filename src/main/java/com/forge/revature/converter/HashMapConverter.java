6851ed8ee2a2240ffb1deefacb0bfb1f81ed125c
ajang06 <aronjang97@gmail.com>
2021-07-28 14:39:43 -0500

    Adds some tests for converterTest

--- src/main/java/com/forge/revature/converter/HashMapConverter.java
+++ src/main/java/com/forge/revature/converter/HashMapConverter.java
@@ -10,30 +10,28 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class HashMapConverter implements AttributeConverter<Map<String, String>, String> {
 
-    @Override
-    public String convertToDatabaseColumn(Map<String, String> flag) {
-    	ObjectMapper objectMapper = new ObjectMapper();
-        String flagInfoJson = null;
-        try {
-            flagInfoJson = objectMapper.writeValueAsString(flag);
-        } catch (final JsonProcessingException e) {
-            //logger.error("JSON writing error", e);
-        }
+	@Override
+	public String convertToDatabaseColumn(Map<String, String> flag) {
+		ObjectMapper objectMapper = new ObjectMapper();
+		String flagInfoJson = null;
+		try {
+			flagInfoJson = objectMapper.writeValueAsString(flag);
+		} catch (final JsonProcessingException e) {
+		}
 
-        return flagInfoJson;
-    }
+		return flagInfoJson;
+	}
 
-    @Override
-    public Map<String, String> convertToEntityAttribute(String flagInfoJSON) {
-    	ObjectMapper objectMapper = new ObjectMapper();
-        Map<String, String> flagInfo = null;
-        try {
-            flagInfo = objectMapper.readValue(flagInfoJSON, Map.class);
-        } catch (final IOException e) {
-            //logger.error("JSON reading error", e);
-        }
+	@Override
+	public Map<String, String> convertToEntityAttribute(String flagInfoJSON) {
+		ObjectMapper objectMapper = new ObjectMapper();
+		Map<String, String> flagInfo = null;
+		try {
+			flagInfo = objectMapper.readValue(flagInfoJSON, Map.class);
+		} catch (final IOException e) {
+		}
 
-        return flagInfo;
-    }
+		return flagInfo;
+	}
 
 }
