8990b548675d4c0290d683f3dae10a3df603c887
FBThornton <97fthornton@gmail.com>
2021-07-30 13:50:24 -0400

    Changed off of RequestMapping

--- src/main/java/com/forge/revature/controllers/ProjectController.java
+++ src/main/java/com/forge/revature/controllers/ProjectController.java
@@ -3,19 +3,20 @@ package com.forge.revature.controllers;
 import java.util.List;
 import java.util.Optional;
 
-import com.forge.revature.models.Project;
-import com.forge.revature.repo.ProjectRepo;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.forge.revature.models.Project;
+import com.forge.revature.repo.ProjectRepo;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -31,27 +32,27 @@ public class ProjectController {
         this.repo = repo;
     }
 
-    @RequestMapping(produces = "application/json", method = RequestMethod.GET)
+    @GetMapping
     public ResponseEntity<List<Project>> allExperience() {
         return new ResponseEntity<>(repo.findAll(), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/{id}", produces = "application/json", method = RequestMethod.GET)
+    @GetMapping("/{id}")
     public ResponseEntity<Optional<Project>> getExperience(@PathVariable(name = "id") long id) {
         return new ResponseEntity<>(repo.findById(id), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/portfolio/{id}", produces = "application/json", method = RequestMethod.GET)
+    @GetMapping("/portfolio/{id}")
     public ResponseEntity<List<Project>> portfolioExperience(@PathVariable(name = "id") int id) {
         return new ResponseEntity<>(repo.findByPortfolio_Id(id), HttpStatus.OK);
     }
 
-    @RequestMapping(consumes = "application/json", method = RequestMethod.POST)
+    @PostMapping
     public void createExperience(@RequestBody Project proj) {
         repo.save(proj);
     }
 
-    @RequestMapping(value = "/{id}", consumes = "application/json", method = RequestMethod.POST)
+    @PostMapping("/{id}")
     public void updateExperience(@PathVariable(name = "id") long id, @RequestBody Project proj) {
         Optional<Project> update = repo.findById(id);
 
@@ -69,7 +70,7 @@ public class ProjectController {
         }
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    @DeleteMapping("/{id}")
     public void deleteExperience(@PathVariable(name = "id") long id) {
         repo.deleteById(id);
     }
