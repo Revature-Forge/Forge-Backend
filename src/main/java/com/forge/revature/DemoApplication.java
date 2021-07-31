8f432b95610d94d708e65939a06a20c07ede7a60
ajang06 <aronjang97@gmail.com>
2021-07-30 19:52:37 -0500

    Add logging using AOP

--- src/main/java/com/forge/revature/DemoApplication.java
+++ src/main/java/com/forge/revature/DemoApplication.java
@@ -2,10 +2,15 @@ package com.forge.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DemoApplication {
+	
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
---
+++ src/main/java/com/forge/revature/aspects/LoggingAspect.java
@@ -0,0 +1,92 @@
+package com.forge.revature.aspects;
+
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * 
+ * This class is an Aspect for logging execution of methods in the Repository
+ * and Controller Components. Logs are currently at the info level, however, for
+ * production, it is recommended to change logging methods at INFO to DEBUG.
+ * 
+ * beanPointcut() - Pointcut that matches all components
+ * packagePointcut() - Pointcut that matches all beans in listed packages
+ * logAfterThrowing() - Advice that logs after a method throws an exception
+ * logAround() - Advice that logs when entering and exiting method 
+ * 
+ * @author Aron Jang
+ *
+ */
+@Aspect
+@Component
+public class LoggingAspect {
+
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+	/**
+	 * Pointcut that matches all Repository Components and Rest Controllers
+	 */
+	@Pointcut("within(@org.springframework.stereotype.Repository *)"
+			+ " || within(@org.springframework.web.bind.annotation.RestController *)")
+	public void beanPointcut() {
+		// PointCuts are empty, implementation is in the advice below
+	}
+
+	/**
+	 * Pointcut that matches all Spring beans in given packages
+	 */
+	@Pointcut("within(com.forge.revature.controllers..*)" + " || within(com.forge.revature.repo..*)")
+	public void packagePointcut() {
+		// PointCuts are empty, implementation is in the advice below
+	}
+
+	/**
+	 * Advice that logs methods when they thrown an exception
+	 *
+	 * @param joinPoint join point for advice
+	 * @param e         exception
+	 */
+	@AfterThrowing(pointcut = "packagePointcut() && beanPointcut()", throwing = "e")
+	public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
+		log.error("Exception in {}.{}() with cause = {}", joinPoint.getSignature().getDeclaringTypeName(),
+				joinPoint.getSignature().getName(), e.getMessage() != null ? e.getMessage() : "login");
+	}
+
+	/**
+	 * Advice that logs when around a method (when the method is entered and exited)
+	 * For now, no other information is displayed
+	 *
+	 * @param joinPoint join point for advice
+	 * @return result
+	 * @throws Throwable throws IllegalArgumentException
+	 */
+	@Around("packagePointcut() && beanPointcut()")
+	public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+
+		log.info("Enter: {}.{}()", joinPoint.getSignature().getDeclaringTypeName(),
+				joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
+
+		try {
+			Object result = joinPoint.proceed();
+
+			log.info("Exit: {}.{}()", joinPoint.getSignature().getDeclaringTypeName(),
+					joinPoint.getSignature().getName());
+
+			return result;
+		} catch (IllegalArgumentException e) {
+			log.error("Illegal argument: {} in {}.{}()", Arrays.toString(joinPoint.getArgs()),
+					joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
+			throw e;
+		}
+	}
+
+}
