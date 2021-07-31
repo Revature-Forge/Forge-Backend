e499823868fba1e195e35019b81d1eb5f664b6e0
ajang06 <aronjang97@gmail.com>
2021-07-30 20:48:55 -0500

    Added more baseline tests for Logging

---
+++ src/test/java/aspectTests/TestLoggingAspect.java
@@ -0,0 +1,52 @@
+package aspectTests;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.forge.revature.aspects.LoggingAspect;
+
+class TestLoggingAspect {
+	LoggingAspect LA;
+	JoinPoint JP;
+	Exception e;
+	ProceedingJoinPoint PJP;
+	
+	@BeforeEach
+	void init() {
+		LA = mock(LoggingAspect.class);
+		JP = mock(JoinPoint.class);
+		e = mock(Exception.class);
+		PJP = mock(ProceedingJoinPoint.class);
+	}
+
+	@Test
+	void beanPointCutReturnsNothing() {
+		LA.beanPointcut();
+		verify(LA, times(1)).beanPointcut();
+	}
+
+	@Test
+	void packagePointCutReturnsNothing() {
+		LA.packagePointcut();
+		verify(LA, times(1)).packagePointcut();
+	}
+
+	@Test
+	void logAfterThrowing() {
+		LA.logAfterThrowing(JP, e);
+		verify(LA, times(1)).logAfterThrowing(JP, e);
+	}
+	
+	@Test
+	void logAroundTest() throws Throwable {
+		LA.logAround(PJP);
+		verify(LA, times(1)).logAround(PJP);
+	}
+
+}
