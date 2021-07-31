f3e4e3a8f72e84b31a687fd1ac87b07dda6675aa
ajang06 <aronjang97@gmail.com>
2021-07-31 12:55:32 -0500

    Combined Logging Aspect Files

--- src/test/java/aspectTests/TestLoggingAspect.java
+++ src/test/java/aspectTests/TestLoggingAspect.java
@@ -1,7 +1,9 @@
 package aspectTests;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -15,8 +17,16 @@ import org.springframework.aop.framework.DefaultAopProxyFactory;
 import com.forge.revature.aspects.LoggingAspect;
 import com.forge.revature.controllers.UserController;
 
+/**
+ * 
+ * Basic testing for Logging Aspect. Used to make sure that exceptions are
+ * thrown
+ * 
+ * @author Aron Jang
+ *
+ */
 class TestLoggingAspect {
-	private final LoggingAspect LA = new LoggingAspect();
+	private LoggingAspect LA = new LoggingAspect();
 	private UserController controllerProxy;
 	JoinPoint JP;
 	Exception e;
@@ -32,12 +42,30 @@ class TestLoggingAspect {
 		controllerProxy = (UserController) aopProxy.getProxy();
 	}
 
-	
 	@Test
 	void testExpectedException() {
 		Assertions.assertThrows(Exception.class, () -> {
 			controllerProxy.getByID(-1);
 		});
 	}
-
+	
+	@Test
+	void logAfterThrowing() {
+		LA = mock(LoggingAspect.class);
+		JP = mock(JoinPoint.class);
+		LA.logAfterThrowing(JP, e);
+		verify(LA, times(1)).logAfterThrowing(JP, e);
+	}
+	
+	@Test
+	void logAroundTest() throws Throwable {
+		LA = mock(LoggingAspect.class);
+		PJP = mock(ProceedingJoinPoint.class);
+		LA.logAround(PJP);
+		verify(PJP, never()).proceed();
+		verify(LA, times(1)).logAround(PJP);
+	}
+	
+	
+	
 }
