a8ab5768bd878e60110490a3e64b88c30251cfe5
Hong Wu <45774204+hwu5542@users.noreply.github.com>
2021-08-20 13:20:26 -0500

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,2 @@
-INSERT INTO users VALUES (1,true,'admin@mail.com','Admin','Account','password');
-INSERT INTO users VALUES (2,false,'test@mail.com','Test','Account','password');
+INSERT INTO USERS VALUES ('1','true','admin@mail.com','Admin','Account','password');
+INSERT INTO USERS VALUES ('2','false','test@mail.com','Test','Account','password');
