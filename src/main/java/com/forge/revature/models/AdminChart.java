4db56d9b7031e659d3a467b4e0a950740e6a6185
Hong Wu <45774204+hwu5542@users.noreply.github.com>
2021-08-23 10:40:21 -0500

    Hong (#3)
    * docker compose url
    
    * admin chart back
    
    * admin chart model, controller and service, user repo and portfolios repo changes
    
    * Update docker-compose.yml
    
    * Update application-production.properties
    
    * Update application.properties
    
    * Update import.sql
    
    * Update import.sql
    
    * Update import.sql
    
    * Update AdminChartController.java
    
    * Update AdminChartService.java
    
    * Update AdminChartService.java
    
    * documentation
    
    * documentation and query fixed
    
    * documentation and query fixed with space
    
    * constructor injection
    
    * private fields
    
    * Update import.sql
    
    * Update import.sql

---
+++ src/main/java/com/forge/revature/models/AdminChart.java
@@ -0,0 +1,16 @@
+package com.forge.revature.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdminChart {
+	String name;
+	int approved;
+	int denied;
+}
