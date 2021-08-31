d1696f27e9bc71d6ead81f61305ae71aa116aa6c
Hong Wu <45774204+hwu5542@users.noreply.github.com>
2021-08-30 21:49:12 -0500

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-INSERT INTO users VALUES (1,false,'hong@mail.com','Hong','Wu','12345678');
-INSERT INTO users VALUES (2,true,'admin@mail.com','Admin','Account','password');
-INSERT INTO users VALUES (3,false,'test@mail.com','Test','Account','password');
-INSERT INTO users VALUES (4,true,'hongadmin@mail.com','Adminhong','Account','password');
-INSERT INTO users VALUES (5,true,'wong@mail.com','Adminwong','Account','password');
-INSERT INTO users VALUES (6,true,'bong@mail.com','Adminbong','Account','password');
-=======
 INSERT INTO USERS VALUES ('1','true','admin@mail.com','Admin','Account','password');
 INSERT INTO USERS VALUES ('2','false','test@mail.com','Test','Account','password');
 INSERT INTO USERS VALUES ('3','false','beachsidedevelopers@gmail.com','Test','Account','password');
 
->>>>>>> 64de1a5db9a3999de3572c3800d055026bca8f76
