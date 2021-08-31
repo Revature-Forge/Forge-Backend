e7053b6179eac061aa9251a86031a5b1738ab662
hwu5542 <hwu5542@gmail.com>
2021-08-30 21:52:29 -0500

    admin report chart junit test done v1

---
+++ src/test/java/com/forge/revature/serviceTests/AdminChartServiceTest.java
@@ -0,0 +1,70 @@
+package com.forge.revature.serviceTests;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.forge.revature.models.AdminChart;
+import com.forge.revature.models.Portfolio;
+import com.forge.revature.models.User;
+import com.forge.revature.repo.PortfolioRepo;
+import com.forge.revature.repo.UserRepo;
+import com.forge.revature.services.AdminChartService;
+
+@SpringBootTest
+public class AdminChartServiceTest {
+
+	@MockBean
+	private UserRepo userRepo;
+
+	@MockBean
+	private PortfolioRepo portfolioRepo;
+	
+	private AdminChartService adminChartService;
+	
+	private User user;
+
+	private Portfolio portfolio;
+	
+	private AdminChart adminChart;
+	
+	private List<User> allAdmin;
+	
+	@BeforeEach
+	public void setup() {
+		adminChartService = new AdminChartService(userRepo, portfolioRepo);
+		user = new User(1, "Hong", "Wu", "hong@mail.com", "password", true);
+		portfolio = new Portfolio(1, "hong Portfolio", user, true, true, true, user, "description", true, true, "", "", 0L, null);
+		adminChart = new AdminChart("Hong Wu", 1, 0);
+		allAdmin = new ArrayList<User>();
+		allAdmin.add(user);
+	}
+
+	@Test
+	public void getApproveCountTest() {
+		given(portfolioRepo.getApproveCount(1)).willReturn(1);
+		
+		assertEquals(1, adminChartService.getCount(true, 1));
+	}
+	
+	@Test
+	public void getDeniedCountTest() {
+		given(portfolioRepo.getDeniedCount(1)).willReturn(0);
+		assertEquals(0, adminChartService.getCount(false, 1));		
+	}
+	
+	@Test
+	public void getAdminListTest() throws Exception {
+		given(userRepo.findAllByAdmin(true)).willReturn(allAdmin);
+		
+		assertEquals("Hong", adminChartService.getAdminList().get(0).getFName());
+	}
+}
